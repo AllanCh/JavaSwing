02ec14f2ad5a2c3fe37a29e5b9fe2cebbf4d290d
AllanCh <edochacon96@gmail.com>
2018-06-22 20:26:46 -0600

    Incluyo los archivos del proyecto por primera vez al repositorio

---
+++ src/graficos/CreaMarcos.java
@@ -0,0 +1,22 @@
+package graficos;
+import javax.swing.*;
+public class CreaMarcos {
+	
+	public static void main (String[] args) {
+		miMarco marco1 = new miMarco();
+		marco1.setVisible(true);
+		marco1.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
+	}
+}
+	
+class miMarco extends JFrame{
+	
+	public miMarco() {
+		setSize(500,300);
+	}
+		
+}
+
+
